e94ed2418461f21176fee30deb698684a051accb
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2024-09-04 14:40:43 +0200

    clear canvas

--- w01p1.js
+++ w01p1.js
@@ -2,11 +2,23 @@
 window.onload = function() { main(); } 
 async function main() 
 { 
-   const adapter = await navigator.gpu.requestAdapter(); 
-   const device = await adapter.requestDevice(); 
-   const canvas = document.getElementById("webgpu-canvas");  
-   const context = canvas.getContext("gpupresent") || canvas.getContext("webgpu");  
-   const canvasFormat = navigator.gpu.getPreferredCanvasFormat(); 
-   context.configure({device: device, format: canvasFormat,   });   
-   // Create a render pass in a command buffer and submit it
+    const adapter = await navigator.gpu.requestAdapter(); 
+    const device = await adapter.requestDevice(); 
+    const canvas = document.getElementById("webgpu-canvas");  
+    const context = canvas.getContext("gpupresent") || canvas.getContext("webgpu");  
+    const canvasFormat = navigator.gpu.getPreferredCanvasFormat(); 
+    context.configure({device: device, format: canvasFormat,   });   
+    // Create a render pass in a command buffer and submit it
+    const encoder = device.createCommandEncoder(); 
+    const pass = encoder.beginRenderPass(
+    { 
+        colorAttachments: [{ 
+        view: context.getCurrentTexture().createView(), 
+        loadOp: "clear", 
+        storeOp:"store",
+        }] 
+    }); 
+    // Insert render pass commands here 
+    pass.end(); 
+    device.queue.submit([encoder.finish()]);
 }
